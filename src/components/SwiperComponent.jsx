30dad379a2ce248c6e962156c9d95137d7f65b77
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2024-01-23 16:30:34 +0100

    home push

---
+++ src/components/SwiperComponent.jsx
@@ -0,0 +1,14 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css'
+import 'swiper/css/effect-coverflow'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+
+import { EffectCoverflow, Pagination, Navigation } from 'swiper'
+
+const SwiperComponent = () => {
+  return <div>SwiperComponent</div>
+}
+
+export default SwiperComponent
